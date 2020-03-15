941e4acbdbe7af0583dd1b1117c2ed25c5d1cca7
yoonjong <yoonjong1820@gmail.com>
2020-03-15 17:30:12 +0900

    added view and template for detailed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # 127.0.0.1:8000/ 로 들어왔을 때 views.post_list를 보여주기.
     # name= 부분은 url에 이름을 붙인 것.
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
